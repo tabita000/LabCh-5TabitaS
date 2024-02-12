5fe078d6bb1fd27b2d8fc4555925dc72e484a6f7
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-02-12 12:24:02 -0600

    Initial Commit

---
+++ LabCh#5TabitaS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabCh#5TabitaS
+//
+//  Created by Tabita Sadiq on 2/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
